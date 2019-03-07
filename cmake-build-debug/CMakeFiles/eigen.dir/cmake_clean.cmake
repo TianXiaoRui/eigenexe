2602cce6248d31349b24ac071730f9fc20c5ea5b
TianXiaoRui <364368795@qq.com>
2019-03-07 21:34:21 +0800

    version_1

---
+++ cmake-build-debug/CMakeFiles/eigen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eigen.dir/eigenexe.cpp.o"
+  "eigen.pdb"
+  "eigen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eigen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/eigenexe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eigenexe.dir/eigenexe.cpp.o"
+  "eigenexe.pdb"
+  "eigenexe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eigenexe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
